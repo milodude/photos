3045cf47ebb8bf49013acfa147bd337b033e09ec
MatiasR <milodude023@gmail.com>
2022-08-22 13:20:41 -0300

    [07]-Added test for failure case when getting photos with use case.

--- test/features/landing_page/domain/use_cases/get_photo_use_case_test.dart
+++ test/features/landing_page/domain/use_cases/get_photo_use_case_test.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:greisy_photos/core/error/faillure.dart';
 import 'package:greisy_photos/core/use_cases/use_case.dart';
 import 'package:greisy_photos/features/landing_page/domain/entities/photo.dart';
 import 'package:greisy_photos/features/landing_page/domain/repository/photo_repository.dart';
@@ -11,8 +12,9 @@ import 'get_photo_use_case_test.mocks.dart';
 
 @GenerateMocks([PhotoRepository])
 void main() {
-  MockPhotoRepository photoRepository = MockPhotoRepository();
-  GetPhotoUseCase useCase = GetPhotoUseCase(photoRepository: photoRepository);
+  MockPhotoRepository mockPhotoRepository = MockPhotoRepository();
+  GetPhotoUseCase useCase =
+      GetPhotoUseCase(photoRepository: mockPhotoRepository);
 
   group('Use case: Get Photos', () {
     test('Should get a list of photos', () async {
@@ -29,7 +31,7 @@ void main() {
         )
       ];
 
-      when(photoRepository.getPhotos())
+      when(mockPhotoRepository.getPhotos())
           .thenAnswer((realInvocation) => Future.value(Right(tPhotoList)));
 
       //Act
@@ -37,7 +39,20 @@ void main() {
 
       //Assert
       expect(result, Right(tPhotoList));
-      verify(photoRepository.getPhotos()).called(1);
+      verify(mockPhotoRepository.getPhotos()).called(1);
+    });
+
+    testWidgets('Should throw a Failure', (tester) async {
+      //ARRANGE
+      const errorMessage = 'Some error';
+      when(mockPhotoRepository.getPhotos()).thenAnswer(((realInvocation) {
+        return Future.value(Left(ServerFailure(errorMessage)));
+      }));
+      //ACT
+      var result = await useCase(NoParams());
+      //ASSERT
+      expect(result, Left(ServerFailure(errorMessage)));
+      verify(mockPhotoRepository.getPhotos()).called(1);
     });
   });
 }
