3f9943c6871e5e639a8080c0d07ffe85a6e71c2f
MatiasR <milodude023@gmail.com>
2022-08-22 17:01:20 -0300

    [12]-Renamed photoEntity for photo

--- lib/features/landing_page/domain/entities/photo.dart
+++ lib/features/landing_page/domain/entities/photo.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
-class PhotoEntity extends Equatable {
+class Photo extends Equatable {
   final String photoId;
   final String photoUrl;
   final String photoDescription;
@@ -10,7 +10,7 @@ class PhotoEntity extends Equatable {
   final String name;
   final String userName;
 
-  const PhotoEntity({
+  const Photo({
     required this.photoId,
     required this.photoUrl,
     required this.photoDescription,
