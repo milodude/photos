db3a6e2ed8a741bbf80530bd44026a45e911acca
MatiasR <milodude023@gmail.com>
2022-08-22 12:09:25 -0300

    [02]-Basic app structure and entity creation

---
+++ lib/features/landing_page/domain/entities/photo.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class PhotoEntity extends Equatable {
+  final String photoId;
+  final String photoUrl;
+  final String photoDescription;
+  final int likes;
+  final String profileImage;
+  final String name;
+  final String userName;
+
+  const PhotoEntity({
+    required this.photoId,
+    required this.photoUrl,
+    required this.photoDescription,
+    required this.likes,
+    required this.profileImage,
+    required this.name,
+    required this.userName,
+  });
+
+  @override
+  List<Object?> get props => [photoId];
+}
