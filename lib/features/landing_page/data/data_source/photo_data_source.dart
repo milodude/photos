d8205576e76d0860a20ca25d1002d5233a17db63
MatiasR <milodude023@gmail.com>
2022-08-22 18:38:41 -0300

    [17]-Photo repository implementation for getting a list of photos.

--- lib/features/landing_page/data/data_source/photo_data_source.dart
+++ lib/features/landing_page/data/data_source/photo_data_source.dart
@@ -1,13 +1,40 @@
+import 'dart:convert';
+
 import 'package:greisy_photos/features/landing_page/data/model/photo_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../core/error/faillure.dart';
+import '../../../../core/providers/url_provider.dart';
 
 abstract class PhotoRepositoryDataSource {
   Future<List<PhotoModel>> getPhotos();
 }
 
 class PhotoDataSource implements PhotoRepositoryDataSource {
+  final http.Client httpClient;
+  final UrlProvider urlProvider;
+
+  PhotoDataSource({required this.httpClient, required this.urlProvider});
+
   @override
-  Future<List<PhotoModel>> getPhotos() {
-    // TODO: implement getPhotos
-    throw UnimplementedError();
+  Future<List<PhotoModel>> getPhotos() async {
+    Uri uri = urlProvider.getUrl('/photos', {});
+
+    var response = await httpClient.get(uri, headers: {
+      'Content-type': 'application/json; charset=utf-8',
+      'Accept': '*/*',
+      'Access-Control-Allow-Origin': '*',
+    });
+    if (response.statusCode == 200) {
+      var decodedJson = json.decode(response.body);
+      List<PhotoModel> clientList = <PhotoModel>[];
+      for (Map<String, dynamic> item in decodedJson) {
+        var client = PhotoModel.fromJson(item);
+        clientList.add(client);
+      }
+      return clientList;
+    } else {
+      throw ServerFailure('Something went wrong while requesting photo list');
+    }
   }
 }
--- test/features/landing_page/data/repository/photo_repository_impl_test.mocks.dart
+++ test/features/landing_page/data/repository/photo_repository_impl_test.mocks.dart
@@ -3,12 +3,14 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i3;
+import 'dart:async' as _i5;
 
+import 'package:greisy_photos/core/providers/url_provider.dart' as _i3;
 import 'package:greisy_photos/features/landing_page/data/data_source/photo_data_source.dart'
-    as _i2;
-import 'package:greisy_photos/features/landing_page/data/model/photo_model.dart'
     as _i4;
+import 'package:greisy_photos/features/landing_page/data/model/photo_model.dart'
+    as _i6;
+import 'package:http/http.dart' as _i2;
 import 'package:mockito/mockito.dart' as _i1;
 
 // ignore_for_file: type=lint
@@ -22,18 +24,39 @@ import 'package:mockito/mockito.dart' as _i1;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
+class _FakeClient_0 extends _i1.SmartFake implements _i2.Client {
+  _FakeClient_0(Object parent, Invocation parentInvocation)
+      : super(parent, parentInvocation);
+}
+
+class _FakeUrlProvider_1 extends _i1.SmartFake implements _i3.UrlProvider {
+  _FakeUrlProvider_1(Object parent, Invocation parentInvocation)
+      : super(parent, parentInvocation);
+}
+
 /// A class which mocks [PhotoDataSource].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockPhotoDataSource extends _i1.Mock implements _i2.PhotoDataSource {
+class MockPhotoDataSource extends _i1.Mock implements _i4.PhotoDataSource {
   MockPhotoDataSource() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i3.Future<List<_i4.PhotoModel>> getPhotos() =>
+  _i2.Client get httpClient =>
+      (super.noSuchMethod(Invocation.getter(#httpClient),
+              returnValue: _FakeClient_0(this, Invocation.getter(#httpClient)))
+          as _i2.Client);
+  @override
+  _i3.UrlProvider get urlProvider =>
+      (super.noSuchMethod(Invocation.getter(#urlProvider),
+              returnValue:
+                  _FakeUrlProvider_1(this, Invocation.getter(#urlProvider)))
+          as _i3.UrlProvider);
+  @override
+  _i5.Future<List<_i6.PhotoModel>> getPhotos() =>
       (super.noSuchMethod(Invocation.method(#getPhotos, []),
               returnValue:
-                  _i3.Future<List<_i4.PhotoModel>>.value(<_i4.PhotoModel>[]))
-          as _i3.Future<List<_i4.PhotoModel>>);
+                  _i5.Future<List<_i6.PhotoModel>>.value(<_i6.PhotoModel>[]))
+          as _i5.Future<List<_i6.PhotoModel>>);
 }
