5e76a08bcd8f56e3d411377dcaeb1bb132ca63de
MatiasR <milodude023@gmail.com>
2022-08-23 15:31:25 -0300

    [26]-Refactor Photo details repository interface.

--- lib/features/photo_details/domain/repository/photo_details_repository.dart
+++ lib/features/photo_details/domain/repository/photo_details_repository.dart
@@ -1,9 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:greisy_photos/features/landing_page/domain/repository/photo_repository.dart';
 
 import '../../../../core/error/faillure.dart';
 import '../../../landing_page/domain/entities/photo.dart';
 
-abstract class PhotoDetailsRepository extends PhotoRepository {
+abstract class PhotoDetailsRepository {
   Future<Either<Failure, Photo>> getPhoto(String photoId);
 }
