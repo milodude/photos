aef54ff7c6ccb73ce7b3a93b92a735abb36b7037
MatiasR <milodude023@gmail.com>
2022-08-24 10:38:10 -0300

    [37]-Added user profile domain entity

---
+++ lib/features/user_profile/domain/entity/user_profile.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class UserProfile extends Equatable {
+  final String userId;
+  final String profileImage;
+  final String name;
+  final String profileDescription; //bio
+  final List<String> photosUrl;
+
+  const UserProfile({
+    required this.userId,
+    required this.profileImage,
+    required this.name,
+    required this.profileDescription,
+    required this.photosUrl,
+  });
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [userId];
+}
